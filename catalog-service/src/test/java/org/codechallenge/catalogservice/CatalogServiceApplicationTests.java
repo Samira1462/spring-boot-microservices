0333532fad50f32543531df679b7a61672688198
samira <samira.radmaneshfar@gmail.com>
2025-05-22 16:30:39 +0200

    setup the test

--- catalog-service/src/test/java/org/codechallenge/catalogservice/CatalogServiceApplicationTests.java
+++ catalog-service/src/test/java/org/codechallenge/catalogservice/CatalogServiceApplicationTests.java
@@ -2,12 +2,12 @@ package org.codechallenge.catalogservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
+@Import(ContainersConfig.class)
 class CatalogServiceApplicationTests {
 
     @Test
-    void contextLoads() {
-    }
-
+    void contextLoads() {}
 }
